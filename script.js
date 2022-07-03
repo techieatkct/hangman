0198a6f336090628f7b654e71f14281a59b9df67
techieatkct <108172869+techieatkct@users.noreply.github.com>
2022-07-03 11:42:21 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+
 const word = document.getElementById('word');
 const incorrect = document.getElementById('incorrect');
 const incorrectLettersEl = document.querySelector('#incorrect p');
